84147f7a430946f03d395ab18d601e37884e1e8d
Claudio M. Antonio <96396925+claudio-m-antonio@users.noreply.github.com>
2023-09-08 01:32:43 -0300

    Add files via upload

--- aplicativos/cursos-online/javascript/06-Contador/app.js
+++ aplicativos/cursos-online/javascript/06-Contador/app.js
@@ -1,29 +1,28 @@
-// set inital value to zero
-let count = 0;
-// select value and buttons
-const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
+let contador = 0;
+const valueElement = document.getElementById("value");
 
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const styles = e.currentTarget.classList;
-    if (styles.contains("decrease")) {
-      count--;
-    } else if (styles.contains("increase")) {
-      count++;
-    } else {
-      count = 0;
-    }
+// Função para atualizar o valor exibido
+function atualizarValor() {
+  valueElement.textContent = contador;
+}
 
-    if (count > 0) {
-      value.style.color = "green";
-    }
-    if (count < 0) {
-      value.style.color = "red";
-    }
-    if (count === 0) {
-      value.style.color = "#222";
-    }
-    value.textContent = count;
-  });
-});
+// Função para diminuir o contador
+function decrementar() {
+  contador--;
+  atualizarValor();
+}
+
+// Função para resetar o contador
+function resetar() {
+  contador = 0;
+  atualizarValor();
+}
+
+// Função para aumentar o contador
+function incrementar() {
+  contador++;
+  atualizarValor();
+}
+
+// Chamar a função para exibir o valor inicial
+atualizarValor();
