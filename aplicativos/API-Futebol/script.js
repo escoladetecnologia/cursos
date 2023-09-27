e23953c3c8cfd3389bc3c2f5d3965919be8f02e8
claudio-m-antonio <upcarreiraclaudio@gmail.com>
2023-09-27 00:49:06 -0300

    atual

--- aplicativos/API-Futebol/script.js
+++ aplicativos/API-Futebol/script.js
@@ -1,17 +1,20 @@
+// script.js
+
 const token = "c49f85b19fbf45fe8bec8a0cda05d2-87";
 const url = "https://api.football-data.org/v4/matches";
 
-fetch(url, {
-  headers: {
-    "X-Auth-Token": token
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    // Lide com os dados da API aqui
-    console.log(data);
-  })
-  .catch(error => {
-    // Lide com erros aqui
-    console.error(error);
-  });
+function buscarPartidas() {
+    fetch(url, {
+        headers: {
+            "X-Auth-Token": token
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const apiDataString = JSON.stringify(data, null, 2);
+        document.getElementById("api-data").textContent = apiDataString;
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
