3060597f05cf16f3c2ba5f96a920cdbe6fbd5fac
claudio-m-antonio <upcarreiraclaudio@gmail.com>
2023-10-11 15:52:31 -0300

    atual 11-10-23

--- aplicativos/cursos-online/javascript/06-Contador/app.js
+++ aplicativos/cursos-online/javascript/06-Contador/app.js
@@ -1,28 +1,21 @@
-let contador = 0;
-const valueElement = document.getElementById("value");
 
-// Função para atualizar o valor exibido
-function atualizarValor() {
-  valueElement.textContent = contador;
-}
+let score1 = 0;
+let score2 = 0;
 
-// Função para diminuir o contador
-function decrementar() {
-  contador--;
-  atualizarValor();
-}
+  function incrementScore(team) {
+    if (team === 1) {
+      score1++;
+      document.getElementById("score1").textContent = score1;
+    } else if (team === 2) {
+      score2++;
+      document.getElementById("score2").textContent = score2;
+    }
+  }
 
-// Função para resetar o contador
-function resetar() {
-  contador = 0;
-  atualizarValor();
-}
+  function resetScores() {
+    score1 = 0;
+    score2 = 0;
+    document.getElementById("score1").textContent = score1;
+    document.getElementById("score2").textContent = score2;
+  }
 
-// Função para aumentar o contador
-function incrementar() {
-  contador++;
-  atualizarValor();
-}
-
-// Chamar a função para exibir o valor inicial
-atualizarValor();
